e7d0a977376e0ddae42b2c75d0c82c4f719089dd
Anton Dutov ERA <dutov,ant@gmail.com>
2021-05-31 14:21:28 +0300

    fulfilled task_3.1

--- lesson_3/task_3.1.py
+++ lesson_3/task_3.1.py
@@ -7,3 +7,28 @@
 Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить информацию, необходимую для перевода:
 какой тип данных выбрать, в теле функции или снаружи.
 """
+
+
+def num_translate(user_answer):
+    for my_key in my_dict:
+        if user_answer == my_key:
+            print(f'{user_answer} - {my_dict.pop(my_key)}')
+            return
+    print(f'{user_answer} - {None}')
+
+
+my_dict = {'one': 'один',
+           'two': 'два',
+           'three': 'три',
+           'four': 'четыре',
+           'five': 'пять',
+           'six': 'шесть',
+           'seven': 'семь',
+           'eight': 'восемь',
+           'nine': 'девять',
+           'ten': 'десять',
+           'zero': 'ноль'}
+
+num_translate(input('Напишите число от 0 до 10 латинскими буквами: '))
+
+
