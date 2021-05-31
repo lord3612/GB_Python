6fcca21ac555a133df3e320b3e5eba5ee3420871
Anton Dutov ERA <dutov,ant@gmail.com>
2021-05-31 15:08:13 +0300

    fulfilled task_3.1 + task_3.2

--- lesson_3/task_3.1.py
+++ lesson_3/task_3.1.py
@@ -6,13 +6,20 @@
 "восемь"
 Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить информацию, необходимую для перевода:
 какой тип данных выбрать, в теле функции или снаружи.
+
+*(вместо задачи 1) Доработать предыдущую функцию в num_translate_adv(): реализовать корректную работу с числительными,
+начинающимися с заглавной буквы — результат тоже должен быть с заглавной.
 """
 
 
 def num_translate(user_answer):
     for my_key in my_dict:
-        if user_answer == my_key:
-            print(f'{user_answer} - {my_dict.pop(my_key)}')
+        if user_answer.istitle() and user_answer == my_key.capitalize():
+            cap = str(my_dict.get(my_key))
+            print(f'{user_answer} - {cap.capitalize()}')
+            return
+        elif user_answer == my_key:
+            print(f'{user_answer} - {my_dict.get(my_key)}')
             return
     print(f'{user_answer} - {None}')
 
@@ -28,7 +35,6 @@ my_dict = {'one': 'один',
            'nine': 'девять',
            'ten': 'десять',
            'zero': 'ноль'}
-
 num_translate(input('Напишите число от 0 до 10 латинскими буквами: '))
 
 
