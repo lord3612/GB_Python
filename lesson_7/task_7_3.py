4b537c3afe144d70a004a2dc2bd6a02cceb5c0af
Anton Dutov <dutov.ant@gmail.com>
2021-06-20 16:32:36 +0300

    edit task_7_3

--- lesson_7/task_7_3.py
+++ lesson_7/task_7_3.py
@@ -23,7 +23,7 @@ new_catalog = 'my_project_new\\templates'
 for root, dirs, files in os.walk('my_project'):
     for file in files:
         if file.endswith(".html"):
-            templates_catalog = os.path.basename(os.path.dirname(os.path.join(root, file)))
+            templates_catalog = os.path.basename(root)
             if not os.path.exists(os.path.join(new_catalog, templates_catalog)):
                 os.makedirs(os.path.join(new_catalog, templates_catalog))
                 shutil.copy(os.path.join(root, file), os.path.join(new_catalog, templates_catalog))
